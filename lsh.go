33fc85e0b7a2aadf9a1d3a5efd0d296163547204
Ian Soboroff <ian.soboroff@nist.gov>
2019-09-23 14:54:10 -0400

    LSH ported

---
+++ lsh.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"log"
+	"math"
+	"strings"
+	"fmt"
+)
+
+type LSH struct {
+	maps []map[string][]string
+	num_rows int
+	num_bands int
+	num_hashes int
+}
+
+func MakeLSH(n, b int) (result LSH) {
+	if (n % b) != 0 {
+		log.Panicf("Bands %d must divide num_hashes %d evenly\n", b, n)
+	}
+
+	result.num_rows = n / b
+	result.num_hashes = n
+	result.num_bands = b
+	result.maps = make([]map[string][]string, result.num_bands)
+	for i := range result.maps {
+		result.maps[i] = make(map[string][]string)
+	}
+	log.Printf("LSH with %d hash buckets and %d bands\n", n, b)
+	log.Printf("Target Jaccard threshold: %.3f\n",
+		math.Pow(1.0 / float64(n), 1.0 / (float64(n) / float64(b))))
+	return result
+}
+	
+func MakeLSHForThreshold(n int, thresh float64) LSH {
+	num_bands := computeBands(n, thresh)
+	return MakeLSH(n, num_bands)
+}
+
+func (h LSH) bandprints(hashes []int) (prints []string) {
+	prints = make([]string, h.num_bands)
+	for b := 0; b < h.num_bands; b++ {
+		var s strings.Builder
+		for r := 0; r < h.num_rows; r++ {
+			fmt.Fprintf(&s, "%x", hashes[b * h.num_rows + r])
+		}
+		prints[b] = s.String()
+	}
+	return prints
+}
+
+func (h LSH) Insert(key string, hashes []int) {
+	prints := h.bandprints(hashes)
+	for b, p := range prints {
+		h.maps[b][p] = append(h.maps[b][p], key)
+	}
+}
+
+func (h LSH) Query(hashes []int) []string {
+	candidates := make(map[string]bool)
+	prints := h.bandprints(hashes)
+	for b, p := range prints {
+		for _, c := range h.maps[b][p] {
+			candidates[c] = true
+		}
+	}
+	result := make([]string, len(candidates))
+	i := 0
+	for c, _ := range candidates {
+		result[i] = c
+		i++
+	}
+	return result
+}
+
+
+func computeBands(num_hashes int, thresh float64) (num_bands int) {
+	var t, last_t float64
+	for num_bands = num_hashes; num_bands > 1; num_bands-- {
+		if ((num_hashes % num_bands) == 0) {
+			x := 1.0 / float64(num_bands)
+			y := float64(num_bands) / float64(num_hashes)
+			last_t = t
+			t = math.Pow(x, y)
+			// log.Printf("With %d hashes, %d bands gives a threshold of %.3f\n", num_hashes, num_bands, t)
+			if t > thresh {
+				break
+			}
+		}
+	}
+	if !(t >= thresh && last_t <= thresh) {
+		log.Printf("Target threshold %.3f not within range [%.3f, %.3f]", thresh, last_t, t)
+	}
+	return num_bands
+}
+
---
+++ lsh_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+	"math"
+)
+
+func TestCompute(t *testing.T) {
+	target := 0.90
+	h := MakeLSHForThreshold(256, target)
+
+	next_band := h.num_bands
+	for next_band++ ; h.num_hashes % next_band != 0; next_band++ {
+	}
+	
+	x0 := 1.0 / float64(next_band)
+	y0 := float64(next_band) / float64(h.num_hashes)
+	low_threshold := math.Pow(x0, y0)
+
+	if low_threshold > target {
+		t.Errorf("Low threshold of %.3f is too high to hit target %.3f",
+			low_threshold, target)
+	}
+	
+	x := 1.0 / float64(h.num_bands)
+	y := float64(h.num_bands) / float64(h.num_hashes)
+	threshold := math.Pow(x, y)
+
+	if threshold < low_threshold {
+		t.Errorf("low_threshold is %.3f, threshold is %.3f",
+			low_threshold, threshold)
+	}
+}
